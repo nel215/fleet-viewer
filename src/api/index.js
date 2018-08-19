fe8115bc4c882fb9231721cbf7aef55234be89b9
nel215 <otomo.yuhei@gmail.com>
2018-08-19 22:22:50 +0900

    Refactor actions

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,6 @@
+import URL from 'url';
+import assert from 'assert';
+
 function parseShip(data) {
   return {
     id: data.api_id,
@@ -12,13 +15,49 @@ function parseShip(data) {
   };
 }
 
+function parseBody(body) {
+  assert(body.search(/^svdata=/) === 0);
+  return JSON.parse(body.substr(7));
+}
+
 export default {
-  createAction(body) {
-    const ships = body.api_data.api_ship_data.map(d => parseShip(d));
-    console.log(ships);
-    return {
-      type: 'handleShipDeck',
-      ships,
-    };
+  createAction(message) {
+    const url = URL.parse(message.url);
+    const body = parseBody(message.body);
+    console.log(url);
+    console.log(body);
+    if (url.pathname === '/kcsapi/api_get_member/questlist') {
+      const quests = body.api_data.api_list.map(d => ({
+        id: d.api_no,
+        title: d.api_title,
+        detail: d.api_detail,
+        state: d.api_state,
+        progress: d.api_progress_flag,
+      }));
+      return {
+        type: 'handleQuestList',
+        quests,
+      };
+    }
+    if (url.pathname === '/kcsapi/api_port/port') {
+      const ships = body.api_data.api_ship.map(d => parseShip(d));
+      const decks = body.api_data.api_deck_port.map(d => ({
+        id: d.api_id,
+        ship_ids: d.api_ship,
+      }));
+      return {
+        type: 'handlePort',
+        ships,
+        decks,
+      };
+    }
+    if (url.pathname === '/kcsapi/api_get_member/ship_deck') {
+      const ships = body.api_data.api_ship_data.map(d => parseShip(d));
+      return {
+        type: 'handleShipDeck',
+        ships,
+      };
+    }
+    return null;
   },
 };
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,26 +1,5 @@
-import URL from 'url';
 import uuid from 'uuid/v4';
-import assert from 'assert';
 import API from '../api';
-
-function parseShip(data) {
-  return {
-    id: data.api_id,
-    ship_id: data.api_ship_id,
-    lv: data.api_lv,
-    hp: data.api_nowhp,
-    maxhp: data.api_maxhp,
-    fuel: data.api_fuel,
-    bull: data.api_bull,
-    cond: data.api_cond,
-    slot: data.api_slot,
-  };
-}
-
-function parseBody(body) {
-  assert(body.search(/^svdata=/) === 0);
-  return JSON.parse(body.substr(7));
-}
 
 export default {
   connect(context) {
@@ -28,47 +7,21 @@ export default {
       name: uuid(),
     });
     port.onMessage.addListener((message) => {
-      const url = URL.parse(message.url);
-      const body = parseBody(message.body);
-      console.log(url);
-      console.log(body);
-      if (url.pathname === '/kcsapi/api_get_member/questlist') {
-        context.dispatch('handleQuestList', {
-          body,
-        });
+      const action = API.createAction(message);
+      if (action === null) {
+        return;
       }
-      if (url.pathname === '/kcsapi/api_port/port') {
-        context.dispatch('handlePort', {
-          body,
-        });
-      }
-      if (url.pathname === '/kcsapi/api_get_member/ship_deck') {
-        context.dispatch(API.createAction(body));
-      }
+      context.dispatch(action);
     });
   },
   handlePort(context, payload) {
-    const ships = payload.body.api_data.api_ship.map(d => parseShip(d));
-    const decks = payload.body.api_data.api_deck_port.map(d => ({
-      id: d.api_id,
-      ship_ids: d.api_ship,
-    }));
-    context.commit('updateShips', { ships });
-    context.commit('updateDecks', { decks });
+    context.commit('updateShips', { ships: payload.ships });
+    context.commit('updateDecks', { decks: payload.decks });
   },
   handleShipDeck(context, payload) {
-    console.log(payload);
     context.commit('updateShips', { ships: payload.ships });
   },
   handleQuestList(context, payload) {
-    console.log(payload);
-    const quests = payload.body.api_data.api_list.map(d => ({
-      id: d.api_no,
-      title: d.api_title,
-      detail: d.api_detail,
-      state: d.api_state,
-      progress: d.api_progress_flag,
-    }));
-    context.commit('updateQuests', { quests });
+    context.commit('updateQuests', { quests: payload.quests });
   },
 };
