51e3c8792120b951e0e86f87bf6cd244b2e9890a
nel215 <otomo.yuhei@gmail.com>
2018-08-19 18:25:11 +0900

    Display attributes of fleet

--- src/view/ship.vue
+++ src/view/ship.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <div v-for="deck in deckShips">
-      <ul>
-        <li v-for="ship in deck">
-          {{ ship.ship_id }}
-        </li>
-      </ul>
-    </div>
+<div class="mdl-card">
+  <div class="mdl-card__title">
+    <h6 class="mdl-card__title-text">Fleets</h6>
   </div>
+  <div class= "mdl-card__supporting-text" v-for="deck in deckShips">
+    <table class="mdl-data-table mdl-shadow--2dp">
+      <thead>
+        <th class="mdl-data-table__cell--non-numeric">Name</th>
+        <th class="mdl-data-table__cell--non-numeric">LV</th>
+        <th class="mdl-data-table__cell--non-numeric">HP</th>
+        <th class="mdl-data-table__cell--non-numeric">Cond</th>
+      </thead>
+      <tr v-for="ship in deck">
+        <td>{{ ship.ship_id }}</td>
+        <td>{{ ship.lv }}</td>
+        <td>{{ ship.hp }}/{{ ship.maxhp }}</td>
+        <td>{{ ship.cond }}</td>
+      </tr>
+    </table>
+  </div>
+</div>
 </template>
 <script src='./ship.js'>
 </script>
