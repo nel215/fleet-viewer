0ea6e108d72550d1f76f601712a0aacd1d5df068
nel215 <otomo.yuhei@gmail.com>
2018-08-22 23:08:52 +0900

    Use table

--- src/view/missions.vue
+++ src/view/missions.vue
@@ -3,9 +3,18 @@
   <div class="mdl-card__title mdl-color--primary mdl-color-text--primary-contrast">
     <h6 class="mdl-card__title-text">Mission</h6>
   </div>
-  <div class= "mdl-card__supporting-text">
-    <div v-for="mission in missions">{{ mission.name }}</div>
-  </div>
+  <table class="mdl-data-table mdl-cell mdl-cell--12-col">
+    <thead>
+      <tr>
+        <th class="mdl-data-table__cell--non-numeric">Name</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="mission in missions">
+        <td class="mdl-data-table__cell--non-numeric">{{ mission.name }}</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 </template>
 <script src='./missions.ts'></script>
