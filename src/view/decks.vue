7786d49ff1c693ab0e0beeb652504c0b58ed5212
nel215 <otomo.yuhei@gmail.com>
2018-08-24 21:29:24 +0900

    Fix tables

--- src/view/decks.vue
+++ src/view/decks.vue
@@ -5,9 +5,7 @@
   </div>
   <mdl-tabs v-model="selected">
     <mdl-tab v-for="deck in decks" :key="deck.id" :tab="{id: deck.id, title: `Deck ${deck.id}`}">
-      <div class="mdl-grid">
-        <ship class="mdl-cell mdl-cell--12-col" :deck-id="deck.id"></ship>
-      </div>
+      <ship class="mdl-cell mdl-cell--12-col" :deck-id="deck.id"></ship>
     </mdl-tab>
   </mdl-tabs>
 </div>
--- src/view/quest.vue
+++ src/view/quest.vue
@@ -3,16 +3,14 @@
   <div class="mdl-card__title mdl-color--primary mdl-color-text--primary-contrast">
     <h6 class="mdl-card__title-text">Quest</h6>
   </div>
-  <div class= "mdl-card__supporting-text">
-    <table class="mdl-data-table">
-      <thead>
-        <th class="mdl-data-table__cell--non-numeric">Title</th>
-      </thead>
-      <tr v-for="quest in selectedQuests">
-        <td class="mdl-data-table__cell--non-numeric">{{ quest.title }}</td>
-      </tr>
-    </table>
-  </div>
+  <table class="mdl-data-table mdl-cell mdl-cell--12-col">
+    <thead>
+      <th class="mdl-data-table__cell--non-numeric">Title</th>
+    </thead>
+    <tr v-for="quest in selectedQuests">
+      <td class="mdl-data-table__cell--non-numeric">{{ quest.title }}</td>
+    </tr>
+  </table>
 </div>
 </template>
 <script src='./quest.ts'></script>
