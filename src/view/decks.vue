9b7fc596860682df45a174068ae6457341dc2a95
nel215 <otomo.yuhei@gmail.com>
2018-08-24 21:31:38 +0900

    Delete title of decks

--- src/view/decks.vue
+++ src/view/decks.vue
@@ -1,14 +1,9 @@
 <template>
-<div>
-  <div class="mdl-card__title mdl-color--primary mdl-color-text--primary-contrast">
-    <h6 class="mdl-card__title-text">Fleets</h6>
-  </div>
-  <mdl-tabs v-model="selected">
-    <mdl-tab v-for="deck in decks" :key="deck.id" :tab="{id: deck.id, title: `Deck ${deck.id}`}">
-      <ship class="mdl-cell mdl-cell--12-col" :deck-id="deck.id"></ship>
-    </mdl-tab>
-  </mdl-tabs>
-</div>
+<mdl-tabs v-model="selected">
+  <mdl-tab v-for="deck in decks" :key="deck.id" :tab="{id: deck.id, title: `Deck ${deck.id}`}">
+    <ship class="mdl-cell mdl-cell--12-col" :deck-id="deck.id"></ship>
+  </mdl-tab>
+</mdl-tabs>
 </template>
 <script src='./decks.ts'>
 </script>
