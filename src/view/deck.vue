813d55ee8efeee33d1d5f6d63d85e1ad99a93562
nel215 <otomo.yuhei@gmail.com>
2018-08-26 11:25:12 +0900

    Fix layout

--- src/view/deck.vue
+++ src/view/deck.vue
@@ -1,20 +1,26 @@
 <template>
-<table class="mdl-data-table">
-  <thead>
-    <tr>
-      <th class="mdl-data-table__cell--non-numeric">Name</th>
-      <th class="mdl-data-table__cell--non-numeric">LV</th>
-      <th class="mdl-data-table__cell--non-numeric">HP</th>
-      <th class="mdl-data-table__cell--non-numeric">Cond</th>
-      <th class="mdl-data-table__cell--non-numeric">Bullet</th>
-      <th class="mdl-data-table__cell--non-numeric">Item</th>
-    </tr>
-  </thead>
-  <tbody>
-    <ship v-for="(shipId, idx) in shipIds" :ship-id="shipId" :key="idx"></ship>
-  </tbody>
-  {{ airPower }}
-</table>
+<div>
+  <table class="mdl-data-table full-width">
+    <thead>
+      <tr>
+        <th class="mdl-data-table__cell--non-numeric">Name</th>
+        <th class="mdl-data-table__cell--non-numeric">LV</th>
+        <th class="mdl-data-table__cell--non-numeric">HP</th>
+        <th class="mdl-data-table__cell--non-numeric">Cond</th>
+        <th class="mdl-data-table__cell--non-numeric">Bullet</th>
+        <th class="mdl-data-table__cell--non-numeric">Item</th>
+      </tr>
+    </thead>
+    <tbody>
+      <ship v-for="(shipId, idx) in shipIds" :ship-id="shipId" :key="idx"></ship>
+    </tbody>
+    <tfoot>
+      <tr>
+        <div class="table-cell">Aerial: {{ airPower }}</div>
+      </tr>
+    </tfoot>
+  </table>
+</div>
 </template>
 <script src='./deck.ts'>
 </script>
